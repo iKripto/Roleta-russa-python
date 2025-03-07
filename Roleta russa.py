7a09d913259551e8b8a2c83353218d376a7ddccd
iKripto <136935247+iKripto@users.noreply.github.com>
2025-03-07 11:17:59 -0300

    Add files via upload

--- Roleta russa.py
+++ Roleta russa.py
@@ -1,19 +1,31 @@
 import random
 
-print("Girando o tamborkkkk")
-num = random.randint(1, 6)
-print(f"O número sorteado foi: {num}")
+while True:
+    print('[1] Roletar')
+    print('[2] Sair')
+    escolha = int(input('Escolha uma opção: '))
 
-if num == 6:
-        print("SE FUDEU KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK")
-        lista = []
-        try:
-            while True:
-                lista.append(bytearray(10**6))  
-        except MemoryError:
-            print("Overflow.")
-else:
-        print("Infelizmente sobreviveu.")
+    if escolha == 1:
+        print("Girando o tamborkkkk")
+        num = random.randint(1, 6)
+        print(f"O número sorteado foi: {num}")
+
+        if num == 6:
+                print("SE FUDEU KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK")
+                lista = []
+                try:
+                    while True:
+                        lista.append(bytearray(10**6))  
+                except MemoryError:
+                    print("Overflow.")
+        else:
+                print("Infelizmente sobreviveu.")
+    
+    else:
+         break
+    
+
+
 
 
 
